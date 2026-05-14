15f37e011c1ac9731f72b89a9d3956d476f86912
arelyweb <arely.web@gmail.com>
2026-05-13 19:51:20 -0600

    created a landing page using tailwild

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, Injectable, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { Beneficios } from './components/beneficios/beneficios';
+import { ComoFunciona } from './components/como-funciona/como-funciona';
+import {ContactForm} from './components/contact-form/contact-form';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [Navbar, Hero, Beneficios, ComoFunciona, ContactForm, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
